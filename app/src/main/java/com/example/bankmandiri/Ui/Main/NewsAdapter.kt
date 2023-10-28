5e9dc6d58de11409440080582258caee88db0956
lenilestari <lenilestari>
2023-10-28 21:36:08 +0700

    Update data get API

--- app/src/main/java/com/example/bankmandiri/Ui/Main/NewsAdapter.kt
+++ app/src/main/java/com/example/bankmandiri/Ui/Main/NewsAdapter.kt
@@ -1,11 +1,17 @@
 package com.example.bankmandiri.Ui.Main
 
 import android.app.DatePickerDialog.OnDateSetListener
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.bankmandiri.Data.Response.ArticlesItem
 import com.example.bankmandiri.Data.Response.NewResponse
 import com.example.bankmandiri.R
@@ -35,14 +41,24 @@ class NewsAdapter (private val listener: (ArticlesItem)-> Unit) : RecyclerView.A
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(news: ArticlesItem) {
-
             itemView.findViewById<TextView>(R.id.tvTitle).text = news.title
+            itemView.findViewById<TextView>(R.id.tvPublishedAt).text = "Published on: ${news.publishedAt}"
 
-            itemView.setOnClickListener {
-                listener(news )
+            val authorTextView = itemView.findViewById<TextView>(R.id.Author)
+            authorTextView.text = "Author: ${news.author}"
+
+            Glide.with(itemView.context)
+                .load(news.urlToImage)
+                .apply(RequestOptions().dontTransform().placeholder(R.drawable.image))
+                .into(itemView.findViewById<ImageView>(R.id.ivNews))
+
+            itemView.findViewById<Button>(R.id.btnReadMore).setOnClickListener {
+                val newsUrl = news.url
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(newsUrl))
+                itemView.context.startActivity(intent)
             }
-
         }
-
     }
+
+
 }
